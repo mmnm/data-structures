39b645c93182eddfc95af3f90764880c0d58c8a0
mmnm <manu.mish@gmail.com>
2015-10-07 16:15:45 -0700

    Finished Pesudoclassical Stack/Queue implementations

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.rear = 0;
+  this.front = 0;
+
 };
 
+Queue.prototype = {
+  constructor: Queue,
 
+  enqueue: function(value) {
+    this.storage[this.rear++] = value;
+  },
+
+  dequeue: function() {
+    if (this.front >= this.rear) {
+    } else {
+      return this.storage[this.front++];
+    }
+  },
+
+  size:  function() {
+    return this.rear - this.front;
+  }
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
 };
 
-
+Stack.prototype = {
+  constructor: Stack,
+  push: function(val) { 
+    this.storage[this.length++] = val;
+  },
+  pop: function() {
+    if (this.length > 0) {
+      return this.storage[--this.length];
+    }
+  },
+  size: function() {
+    return this.length;
+  }
+}
