652ad037cea6dd817ef7dca89e987d7840910a25
mmnm <manu.mish@gmail.com>
2015-10-07 14:53:14 -0700

    Preparing for merge

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
   var someInstance = {};
+  var front = 0;
+  var rear = 0;
+
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -13,6 +16,7 @@ var Queue = function() {
   };
 
   someInstance.size = function() {
+    return rear - front;
   };
 
   return someInstance;
