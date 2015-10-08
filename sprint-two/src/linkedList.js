ac0e80f281dfc2e61f19eb09d25154b22cfa6f44
mmnm <manu.mish@gmail.com>
2015-10-08 10:39:54 -0700

    Completed List implementations

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    var temp = new Node(value);
+    if(list.tail !== null) {
+      list.tail.next = temp;
+      list.tail = temp;
+    } else {
+      list.tail = list.head = temp;
+    }
+
   };
 
   list.removeHead = function() {
+    if(list.head !== null) {
+      var temp = list.head;
+      if(list.head === list.tail) {
+        list.head = null;
+        list.tail = null;
+        
+      } else {
+        list.head = list.head.next;
+      }
+      return temp.value;
+    }
   };
 
   list.contains = function(target) {
+    var temp = list.head;
+
+    while(temp !== null) {
+      if(temp.value === target) {
+        return true;
+      }
+
+      temp = temp.next;
+    }
+    return false;
   };
 
   return list;
