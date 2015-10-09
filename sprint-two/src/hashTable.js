cc78170b3cc2ab243b5289bc8c19075a24b91816
mmnm <manu.mish@gmail.com>
2015-10-08 20:00:00 -0700

    Broken Hash Table partial code

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,37 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var cur = this._storage.get(index);
+  if(cur != "undefined") {
+    cur.push(v);
+  }
+
+  this._storage.set(index, cur);
+/*
+  var currentVal = this._storage.get(index);
+  if (currentVal === undefined) {
+    this._storage.set(index, v);
+  } else {
+    this._storage.set(index, [].concat(currentVal, v));
+  }*/
+
+
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
+  
+  var currentVal = this._storage.get(index);
+  if (Array.isArray(currentVal)) {
+    currentVal.indexOf('k')
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, null);
 };
 
 
