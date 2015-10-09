f3fdcbae5bba6f72e800c570a9edbe09da5224f9
mmnm <manu.mish@gmail.com>
2015-10-09 11:38:36 -0700

    Completed Hash Table implementations -

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,41 +3,63 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.findIndexForKey = function(bucket, k) {
+    var indexFound = -1;
+    for (var i = 0; i < bucket.length; i += 2) { 
+      if (bucket[i] === k) {
+        indexFound = i;
+        break;
+      }
+    }
+    return indexFound;
+
+  }
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var cur = this._storage.get(index);
-  if(cur != "undefined") {
-    cur.push(v);
-  }
-
-  this._storage.set(index, cur);
-/*
-  var currentVal = this._storage.get(index);
-  if (currentVal === undefined) {
-    this._storage.set(index, v);
+  var bucket = this._storage.get(index);
+  if (bucket === undefined) {
+    this._storage.set(index,[k,v]);
   } else {
-    this._storage.set(index, [].concat(currentVal, v));
-  }*/
-
-
-
+    var status = false;
+    for (var i = 0; i < bucket.length; i += 2) {
+      if (bucket[i] === k) {
+        status = true;
+        bucket[i + 1] = v;
+      }
+    }
+    if(!status) {
+      bucket.push(k,v);
+    }
+    this._storage.set(index,bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
-  
-  var currentVal = this._storage.get(index);
-  if (Array.isArray(currentVal)) {
-    currentVal.indexOf('k')
+  var bucket = this._storage.get(index);
+
+  for (var i = 0; i < bucket.length; i += 2) { 
+    if (bucket[i] === k) {
+      return bucket[i + 1];
+    }
   }
+  return null;
 };
+
+
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, null);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i += 2) {
+    if (bucket[i] === k) {
+      bucket.splice(i,2);
+      break;
+    }
+  }
+  this._storage.set(index,bucket);
 };
 
 
